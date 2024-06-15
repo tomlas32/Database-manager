3e52876be4bd47371c1e24c20e9d46fb358ed2c9
tlasota <t.lasota@erba.com>
2024-06-15 16:59:50 +0100

    added get_database and list_collection functions to be able to fetch the relevant details upon application start up

--- database.py
+++ database.py
@@ -4,8 +4,11 @@ import credentials as cr
 client = pymongo.MongoClient(f"mongodb+srv://{cr.USER_NAME}:{cr.PASSWORD}{cr.MONGO_URL[0]}")
 
 # function for fetching available databases
-def get_database():
-    return list(client.list_database_names())
+def get_database(client):
+    exclude_databases = ["admin", "local", "config"]
+    db_names = client.list_database_names()
+    user_created_dbs = [db for db in db_names if db not in exclude_databases]
+    return user_created_dbs
 
 # list collections
 def list_collections(database_name):

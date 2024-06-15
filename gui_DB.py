ce81f804fecc273786858fde23055506b302c983
tlasota <t.lasota@erba.com>
2024-06-15 11:27:52 +0100

    first commit

---
+++ gui_DB.py
@@ -0,0 +1,58 @@
+import numpy as np
+import pyqtgraph as pgt
+from PyQt5.QtCore import QTimer, QObject, pyqtSignal, Qt
+from PyQt5.QtWidgets import QApplication, QVBoxLayout, QHBoxLayout, QFileDialog, QMainWindow, QWidget, QPushButton, QComboBox, QMessageBox, QLabel, QLCDNumber, QLineEdit
+from PyQt5.QtWidgets import QSizePolicy, QSpacerItem, QTextEdit
+from PyQt5.QtGui import QIcon
+import credentials as cr
+
+class DatabaseManager(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+
+        ######################## specify basic resources
+        main_icon = QIcon("D:\Projects\Work projects\Database-manager\\assets\main-icon.ico")
+        
+        ######################## specify basic main window configurations
+        self.setWindowTitle("Database Manger")
+        self.setWindowIcon(main_icon)
+        self.setFixedSize(1024, 500)
+
+        ######################## specify layouts
+        self.main_layout = QVBoxLayout()
+        self.db_layout = QHBoxLayout()
+        self.query_layout = QHBoxLayout()
+        self.result_layout = QHBoxLayout()
+        self.buttons_layout = QHBoxLayout()
+
+        ######################## specify widgets and add to layouts
+        # main widget and layouts
+        self.window = QWidget()
+        self.window.setLayout(self.main_layout)
+        self.setCentralWidget(self.window)
+        self.main_layout.addLayout(self.db_layout)
+        self.main_layout.addLayout(self.query_layout)
+        self.main_layout.addLayout(self.result_layout)
+        self.main_layout.addLayout(self.buttons_layout)
+
+        # create db widgets and add to layouts
+        self.url_label = QLabel("Database URL:")
+        self.url_input = QLineEdit(cr.MONGO_URL)
+        self.url_input.setFixedWidth(510)
+        self.db_label = QLabel("Database:")
+        self.db_input = QComboBox()
+        self.db_input.setFixedWidth(140)
+        self.collection_label = QLabel("Collection:")
+        self.collection_input = QComboBox()
+        self.collection_input.setFixedWidth(140)
+        self.db_layout.addWidget(self.url_label)
+        self.db_layout.addWidget(self.url_input)
+        self.db_layout.addWidget(self.db_label)
+        self.db_layout.addWidget(self.db_input)
+        self.db_layout.addWidget(self.collection_label)
+        self.db_layout.addWidget(self.collection_input)
+
+
+
+
---
+++ main.py
@@ -0,0 +1,12 @@
+from gui_DB import DatabaseManager
+from PyQt5.QtWidgets import QApplication
+import sys
+
+
+
+
+if __name__ == "__main__":
+    application = QApplication(sys.argv)                                                            # creates instance of QApplication
+    main_window = DatabaseManager()
+    main_window.show()
+    sys.exit(application.exec_())

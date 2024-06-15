c17b8ad108d93bd50b3ae71bacdd08aaf5fd9ac9
tlasota <t.lasota@erba.com>
2024-06-15 17:25:37 +0100

    corrected get_database function to only retrive user created databases

--- database.py
+++ database.py
@@ -4,7 +4,7 @@ import credentials as cr
 client = pymongo.MongoClient(f"mongodb+srv://{cr.USER_NAME}:{cr.PASSWORD}{cr.MONGO_URL[0]}")
 
 # function for fetching available databases
-def get_database(client):
+def get_database():
     exclude_databases = ["admin", "local", "config"]
     db_names = client.list_database_names()
     user_created_dbs = [db for db in db_names if db not in exclude_databases]
